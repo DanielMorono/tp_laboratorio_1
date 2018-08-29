11f4b0f07e82d7133dcc8ae552ccade584d0df7f
Daniel <morono.daniel99@gmail.com>
2018-08-29 19:58:37 -0300

    3 Funciones terminadas

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int sumar (int , int);
+int restar (int,int);
+float dividir (int,int);
 int main()
 {
     int opcion;
@@ -28,6 +30,11 @@ int main()
             printf("Ingrese otro numero: \n");
             scanf("%d", & numeroDos);
             printf("Estoy sumando %d + %d\n", numeroUno,numeroDos);
+            int suma;
+            suma = sumar(numeroUno,numeroDos);
+
+            printf("La suma es: %d \n",suma);
+
             break;
         case 2:
             printf("Ingrese un numero: \n");
@@ -36,6 +43,12 @@ int main()
             printf("Ingrese otro numero: \n");
             scanf("%d", & numeroDos);
             printf("Estoy restando %d - %d\n", numeroUno,numeroDos);
+
+            int resta;
+
+            resta = restar (numeroUno , numeroDos);
+
+            printf("La resta es: %d \n",resta);
             break;
         case 3:
             printf("Ingrese un numero: \n");
@@ -44,6 +57,11 @@ int main()
             printf("Ingrese otro numero: \n");
             scanf("%d", & numeroDos);
             printf("Estoy dividiendo %d / %d\n", numeroUno,numeroDos);
+            float division;
+
+            division= dividir(numeroUno, numeroDos);
+
+            printf("La division es: %.2f\n",division);
             break;
         case 4:
             printf("Ingrese un numero: \n");
@@ -75,4 +93,31 @@ int main()
     while (opcion != 6);
 
     return 0;
+}
+
+int sumar(int numeroUno, int numeroDos)              //FUNSION SUMA
+{
+    int suma;
+
+    suma = numeroUno + numeroDos;
+
+    return suma;
+}
+
+int restar (int numeroUno, int numeroDos)          //FUNSION RESTA
+{
+    int resta;
+
+    resta = numeroUno - numeroDos;
+
+    return resta;
+}
+
+float dividir(int numeroUno, int numeroDos)        //FUNSION DIVISION
+{
+    float division;
+
+    division = (float)numeroUno / (float)numeroDos;
+
+    return division;
 }
